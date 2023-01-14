732e9157217ab07c22bc47e4efb8f20d1a91e9b4
jrobalino <javier.robalino@gmail.com>
2023-01-13 20:46:51 -0600

    Add TypeScript.

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
+import * as https from 'https';
+import * as dotenv from 'dotenv';
+
 require('dotenv').config();
 
-const readmeApiKey = btoa(process.env.README_API_KEY);
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const devPortalDomain = 'https://dev.frontapp.com/docs/';
+const readmeApiKey: string = btoa(process.env.README_API_KEY!);
+const clientId: string = process.env.CLIENT_ID!;
+const clientSecret: string = process.env.CLIENT_SECRET!;
+const devPortalDomain: string = 'https://dev.frontapp.com/docs/';
 
-const https = require('https');
+interface Doc {
+  url: string;
+  title: string;
+  content: string;
+  source: string;
+}
 
-let oauthToken = '';
+interface Category {
+  slug: string;
+}
 
-async function getCategories() {
+let oauthToken: string = '';
+
+async function getCategories(): Promise<string[]> {
 // Retrieve all the Developer Portal documentation categories from Readme
 // https://docs.readme.com/main/reference/getcategories
   console.log('Getting all categories from Readme...');
@@ -34,7 +46,7 @@ async function getCategories() {
 
       res.on('end', () => {
         try {
-          const jsonData = JSON.parse(data);
+          const jsonData: Category[] = JSON.parse(data);
           const slugs = jsonData.map((obj) => obj.slug);
           resolve(slugs);
         } catch (error) {
@@ -51,13 +63,13 @@ async function getCategories() {
   });
 };
 
-async function getDocSlugs(categories) {
+async function getDocSlugs(categories: string[]): Promise<string[]> {
 // Get the doc slugs for each category in Readme
 // https://docs.readme.com/main/reference/getcategorydocs
 
   console.log('Getting all docs from each category...');
 
-  const results = [];
+  const results: string[] = [];
 
   const dataPromises = categories.map((category) => {
     return new Promise((resolve, reject) => {
@@ -97,18 +109,18 @@ async function getDocSlugs(categories) {
     });
   });
 
-  const dataArrays = await Promise.all(dataPromises);
-  dataArrays.forEach((dataArray) => {
+  const dataArrays = await Promise.all(dataPromises) as Array<string[]>;
+  dataArrays.forEach((dataArray: string[]) => {
     results.push(...dataArray);
   });
 
   return results;
 };
 
-function getSlugsRecursive(jsonArray) {
+function getSlugsRecursive(jsonArray: any[]): string[] {
 // Check if the JSON object has children with "slug" keys
 
-  let slugs = [];
+  let slugs: string[] = [];
 
   jsonArray.forEach((json) => {
     if (json.slug) {
@@ -123,15 +135,15 @@ function getSlugsRecursive(jsonArray) {
   return slugs;
 }
 
-async function getDocs(slugs) {
+async function getDocs(slugs: string[]): Promise<Doc[]> {
 // Get the title, url, and body content for each doc based on its slug
 // https://docs.readme.com/main/reference/getdoc
 
   console.log('Getting doc contents...');
 
-  const results = [];
+  const results: Doc[] = [];
 
-  const dataPromises = slugs.map((slug) => {
+  const dataPromises = slugs.map((slug: string) => {
     return new Promise((resolve, reject) => {
       const options = {
         hostname: 'dash.readme.com',
@@ -153,7 +165,7 @@ async function getDocs(slugs) {
         res.on('end', () => {
           try {
             const jsonData = JSON.parse(data);
-            const mappedData = {
+            const mappedData: Doc = {
               url: devPortalDomain + slug,
               title: jsonData.title,
               content: jsonData.body || jsonData.excerpt || jsonData.title || 'Developer Portal',
@@ -174,15 +186,15 @@ async function getDocs(slugs) {
     });
   });
 
-  const dataArrays = await Promise.all(dataPromises);
-  dataArrays.forEach((dataArray) => {
+  const dataArrays = await Promise.all(dataPromises) as Doc[];
+  dataArrays.forEach((dataArray: Doc) => {
     results.push(dataArray);
   });
 
   return results;
 };
 
-async function getAuthToken(clientId, clientSecret) {
+async function getAuthToken(clientId: string, clientSecret: string): Promise<string> {
 // Retrieve an OAuth2 access token with write permissions from the Insided API
 // https://api2-eu-west-1.insided.com/docs/#section/Authentication
 
@@ -225,7 +237,7 @@ async function getAuthToken(clientId, clientSecret) {
   });
 };
 
-async function deleteSearchIndex(token) {
+async function deleteSearchIndex(token: string): Promise<void> {
 // Delete the current Developer Portal docs index for the Insided federated search
 // https://api2-eu-west-1.insided.com/docs/search/#operation/clear
 
@@ -259,7 +271,7 @@ async function deleteSearchIndex(token) {
   });
 };
 
-async function updateSearchIndex(token, data) {
+async function updateSearchIndex(token: string, data: Doc[]): Promise<void> {
 // Index the Developer Portal docs for the Insided federated search
 // https://api2-eu-west-1.insided.com/docs/search/#operation/index
 
@@ -300,13 +312,13 @@ async function updateSearchIndex(token, data) {
 // Get all categories from readme, then get all docs for each category, then get title, URLs, and body content for each doc
 // Delete the Insided federated search index for the Developer Portal and then upload the latest content
 
-async function processData() {
+async function processData(): Promise<void> {
   try {
-    const token = await getAuthToken(clientId, clientSecret);
+    const token: string = await getAuthToken(clientId, clientSecret);
     oauthToken = token;
-    const categories = await getCategories();
-    const slugs = await getDocSlugs(categories);
-    const docs = await getDocs(slugs);
+    const categories: string[] = await getCategories();
+    const slugs: string[] = await getDocSlugs(categories);
+    const docs: Doc[] = await getDocs(slugs);
     await deleteSearchIndex(oauthToken);
     await updateSearchIndex(token, docs);
   } catch (error) {
